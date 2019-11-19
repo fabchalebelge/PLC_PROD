7bb34cc7dd90fb00e8861e588449729ca2da2233
Fabian Chapelle <fchapelle@torsen.be>
2019-11-19 08:20:13 +0100

    Ajout ON DELETE CASCADE sur les clés étrangères Part et Measurement

--- PLC_PROD/USI_Part.sql
+++ PLC_PROD/USI_Part.sql
@@ -4,5 +4,5 @@
     [workOrderId] INT NOT NULL, 
     [timeStamp] DATETIME NULL DEFAULT GetDate(), 
     [valid] BIT NULL, 
-    CONSTRAINT [FK_Part_WorkOrder] FOREIGN KEY ([workOrderId]) REFERENCES [USI_WorkOrder]([id])
+    CONSTRAINT [FK_Part_WorkOrder] FOREIGN KEY ([workOrderId]) REFERENCES [USI_WorkOrder]([id]) ON DELETE CASCADE
 )
