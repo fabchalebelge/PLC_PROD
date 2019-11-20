7e0dc7dc3f89a5c0269ada054791d56393244bc5
Fabian Chapelle <fabian.chapelle@hotmail.be>
2019-11-20 15:43:48 +0100

    Ajout trigger Part (for delete, insert, update)

--- PLC_PROD/USI_Part.sql
+++ PLC_PROD/USI_Part.sql
@@ -6,3 +6,52 @@
     [valid] BIT NULL, 
     CONSTRAINT [FK_Part_WorkOrder] FOREIGN KEY ([workOrderId]) REFERENCES [USI_WorkOrder]([id])
 )
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_Part]
+    ON [dbo].[USI_Part]
+    FOR DELETE, INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON;
+
+		DECLARE
+			@workOrderId int,
+			@numOfParts smallint,
+			@numOfPartsOk smallint,
+			@numOfPartsNok smallint,
+			@timeStampStop datetime,
+			@completed bit;
+
+		SET @workOrderId	= COALESCE((SELECT [workOrderId] FROM [inserted]), (SELECT [workOrderId] FROM [inserted]));
+
+		SET @timeStampStop	= GETDATE();
+
+		SELECT
+			@numOfParts		= COUNT([id]),
+			@numOfPartsOk	= SUM([valid]),
+			@numOfPartsNok	= COUNT([valid]) - SUM([valid])
+		FROM
+			[USI_Part]
+		WHERE
+			[workOrderId] = @workOrderId;
+
+		IF @numOfPartsOk >= (SELECT [size] FROM [USI_WorkOrder] WHERE [id] = @workOrderId)
+			SET @completed = 1;
+		ELSE
+			SET @completed = 0;
+
+
+		UPDATE
+			[USI_WorkOrder]
+		SET
+			[timeStampStop]	= @timeStampStop,
+			[numOfParts]	= @numOfParts,
+			[numOfPartsOk]	= @numOfPartsOk,
+			[numOfPartsNok]	= @numOfPartsNok,
+			[completed]		= @completed
+		WHERE
+			[id] = @workOrderId;
+    END
+GO
