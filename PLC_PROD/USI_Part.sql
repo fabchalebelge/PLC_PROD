6b48d576195132911af33209404a3f9952668c7b
Fabian Chapelle <fchapelle@torsen.be>
2019-11-21 07:45:50 +0100

    Modification du trigger Part pour exécution uniquement si une seule part (cas du delete WorkOrder -> suppression de plusieurs parts)

--- PLC_PROD/USI_Part.sql
+++ PLC_PROD/USI_Part.sql
@@ -23,35 +23,38 @@ CREATE TRIGGER [dbo].[Trigger_Part]
 			@numOfPartsNok smallint,
 			@timeStampStop datetime,
 			@completed bit;
+		
+		IF (SELECT COUNT(*) FROM [inserted]) = 1 OR (SELECT COUNT(*) FROM [deleted]) = 1	--Utile lors de la suppression d'un OF complet (ON DELETE CASCADE)
+			BEGIN
+				SET @workOrderId	= COALESCE((SELECT [workOrderId] FROM [inserted]), (SELECT [workOrderId] FROM [deleted]));
 
-		SET @workOrderId	= COALESCE((SELECT [workOrderId] FROM [inserted]), (SELECT [workOrderId] FROM [deleted]));
+				SET @timeStampStop	= GETDATE();
 
-		SET @timeStampStop	= GETDATE();
+				SELECT
+					@numOfParts		= COUNT([id]),
+					@numOfPartsOk	= SUM(CAST([valid] AS tinyint)),
+					@numOfPartsNok	= COUNT([valid]) - SUM(CAST([valid] AS tinyint))
+				FROM
+					[USI_Part]
+				WHERE
+					[workOrderId] = @workOrderId;
 
-		SELECT
-			@numOfParts		= COUNT([id]),
-			@numOfPartsOk	= SUM(CAST([valid] AS tinyint)),
-			@numOfPartsNok	= COUNT([valid]) - SUM(CAST([valid] AS tinyint))
-		FROM
-			[USI_Part]
-		WHERE
-			[workOrderId] = @workOrderId;
-
-		IF @numOfPartsOk >= (SELECT [size] FROM [USI_WorkOrder] WHERE [id] = @workOrderId)
-			SET @completed = 1;
-		ELSE
-			SET @completed = 0;
+				IF @numOfPartsOk >= (SELECT [size] FROM [USI_WorkOrder] WHERE [id] = @workOrderId)
+					SET @completed = 1;
+				ELSE
+					SET @completed = 0;
 
 
-		UPDATE
-			[USI_WorkOrder]
-		SET
-			[timeStampStop]	= @timeStampStop,
-			[numOfParts]	= @numOfParts,
-			[numOfPartsOk]	= @numOfPartsOk,
-			[numOfPartsNok]	= @numOfPartsNok,
-			[completed]		= @completed
-		WHERE
-			[id] = @workOrderId;
+				UPDATE
+					[USI_WorkOrder]
+				SET
+					[timeStampStop]	= @timeStampStop,
+					[numOfParts]	= @numOfParts,
+					[numOfPartsOk]	= @numOfPartsOk,
+					[numOfPartsNok]	= @numOfPartsNok,
+					[completed]		= @completed
+				WHERE
+					[id] = @workOrderId;
+			END
     END
 GO
